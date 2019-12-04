88b374bdce90b9f3780e7e64e589f4792dc96b48
Alisha Smith <33044111+alishabelle@users.noreply.github.com>
2019-12-04 03:29:19 -0500

    updating files for new changes

---
+++ pop.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener("click", function() {
+    document.querySelector('.pop').getElementsByClassName.display = "flex";
+});
+document.querySelector('.close').addEventListener("click", function() {
+    document.querySelector('.pop').getElementsByClassName.display = "none";
+});
