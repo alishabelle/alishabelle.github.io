124cce722487222314c2dc3541d953354cbff933
Alisha Smith <33044111+alishabelle@users.noreply.github.com>
2019-12-05 14:49:48 -0500

    updating js file

--- pop.js
+++ pop.js
@@ -1,6 +1,18 @@
-document.getElementById('button').addEventListener("click", function() {
-    document.querySelector('.pop').getElementsByClassName.display = "flex";
+document.getElementById('button1').addEventListener('click', function() {
+    document.querySelector('.popup1').style.display = "flex";
 });
 document.querySelector('.close').addEventListener("click", function() {
-    document.querySelector('.pop').getElementsByClassName.display = "none";
+    document.querySelector('.popup1').style.display = "none";
+});
+document.getElementById('button2').addEventListener('click', function() {
+    document.querySelector('.popup2').style.display = "flex";
+});
+document.querySelector('.close1').addEventListener("click", function() {
+    document.querySelector('.popup2').style.display = "none";
+});
+document.getElementById('button3').addEventListener('click', function() {
+    document.querySelector('.popup3').style.display = "flex";
+});
+document.querySelector('.close2').addEventListener("click", function() {
+    document.querySelector('.popup3').style.display = "none";
 });
